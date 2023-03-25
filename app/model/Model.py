14d5ef2cdd1e19c77386a8e14c4f9ebc32587f05
Monyz <mamy32011@gmail.com>
2023-03-25 14:31:45 +0300

    Created first model of database

--- app/model/Model.py
+++ app/model/Model.py
@@ -1,1 +1,32 @@
+from peewee import *
 
+db = SqliteDatabase('HoshHongAlice.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Users(BaseModel):
+    id = IntegerField(primary_key=True)
+    user_id = TextField(unique=True)
+    task = IntegerField()
+
+    class Meta:
+        table_name = "Пользователи"
+
+
+class Tasks(BaseModel):
+    id = IntegerField(primary_key=True)
+    text = TextField()
+    image = TextField()
+    answer = TextField()
+    explanation = TextField()
+    topic = TextField()
+
+    class Meta:
+        table_name = "Задания"
+
+
+Tasks.create_table()
