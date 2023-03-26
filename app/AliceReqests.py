06ceba1dbc56d76a70ce9b9503bf1b5f481dc7aa
Matthew <prokofiev.matvei@gmail.com>
2023-03-26 20:21:50 +0300

    update

--- app/AliceReqests.py
+++ app/AliceReqests.py
@@ -1,6 +1,5 @@
 import json
 
-
 class AliceRequest(object):  # запрос
     def __init__(self, request_dict):
         self._request_dict = request_dict
@@ -14,8 +13,24 @@ class AliceRequest(object):  # запрос
         return self._request_dict['session']
 
     @property
+    def session_state(self):
+        if 'session_state' in self._request_dict.keys():
+            return self._request_dict['session_state']
+        return None
+
+    @property
+    def session_state_value(self):
+        if self.session_state is None:
+            return None
+        return self.session_state['value']
+
+    @property
     def user_id(self):
         return self.session['user_id']
+
+    @property
+    def is_screen(self):
+        return 'screen' in self._request_dict['meta']['interfaces'].keys()
 
     @property
     def is_new_session(self):
@@ -50,11 +65,34 @@ class AliceResponse(object):  # ответ
             indent=2
         )
 
-    def set_text(self, text):
-        self._response_dict['response']['text'] = text[:1024]
+    def set_text(self, text, tts=False):
+        if tts:
+            self._response_dict['response']['text'] = text[:1024]
+            self._response_dict['response']['tts'] = text[:1024]
+        else:
+            self._response_dict['response']['text'] = text[:1024]
 
-    def set_buttons(self, buttons):
-        self._response_dict['response']['buttons'] = buttons
+    def set_value(self, value):
+        self._response_dict["session_state"] = {}
+        self._response_dict["session_state"]["value"] = value
+
+
+
+    def add_button(self, button):
+        if 'buttons' not in self._response_dict['response'].keys():
+            self._response_dict['response']['buttons'] = []
+            self._response_dict['response']['buttons'].append({"title": button})
+        else:
+            self._response_dict['response']['buttons'].append({"title": button})
+
+    def add_payload_to_button(self, index_of_button: int, payload: dict):
+        self._response_dict['response']['buttons'][index_of_button]['payload'] = payload
+
+    def add_url_to_button(self, index_of_button: int, url: str):
+        self._response_dict['response']['buttons'][index_of_button]['url'] = url
+
+    def add_hide_to_button(self, index_of_button: int, hide: bool):
+        self._response_dict['response']['buttons'][index_of_button]['hide'] = hide
 
     def end(self):
         self._response_dict["response"]["end_session"] = True
--- app/Controller.py
+++ app/Controller.py
@@ -4,5 +4,11 @@ from AliceReqests import AliceResponse, AliceRequest
 def handler(event, context):
     request = AliceRequest(event)
     response = AliceResponse(request)
-    response.set_text("Ты крутой!!!")
+    if request.is_new_session:
+        response.set_text(
+            "Привет! Ты попал в навык «Реши задачу»! Возьми ручку и тетрадку, и давай решать задачи. Начинаем?",
+            tts=True)
+        response.add_button('Да')
+        response.add_button('Нет')
+        response[]
     return response._response_dict
